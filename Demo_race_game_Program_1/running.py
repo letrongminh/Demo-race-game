4eb3c3ac96dc08f8b82338c8df29ccc0243bd029
Ле Чонг Минь <59441846+letrongminh@users.noreply.github.com>
2021-03-26 13:30:03 +0300

    update

--- Demo_race_game_Program_1/running.py
+++ Demo_race_game_Program_1/running.py
@@ -1,1 +1,8 @@
-exec(open('main.py').read())
+# exec(open('main.py').read())
+# exec(open('race_game.py').read())
+
+from subprocess import *
+import time
+Popen('python main.py')
+# time.sleep(1)
+Popen('python race_game.py')
