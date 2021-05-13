25ca4fee0761cda3b4193366a24d38581d9ea406
letrongminh <kimchon999555@gmail.com>
2021-05-14 01:18:41 +0300

    Set the photo frame in the frame, edit the button path and widget

--- Demo_race_game_Program_1/take_photos.py
+++ Demo_race_game_Program_1/take_photos.py
@@ -4,17 +4,20 @@ import os
 import tkinter
 import PIL.Image
 import PIL.ImageTk
+file_path = os.path.dirname(__file__)
+gallery_path = os.path.join(file_path, "gallery")
+hand_path = os.path.join(gallery_path, "hand_templates")
+
 
 main_windows = tkinter.Tk(className='open camera')
 camera = cv2.VideoCapture(0)
-# main_windows.geometry('80x80')
+
 frame_w = camera.get(cv2.CAP_PROP_FRAME_WIDTH) // 2
 frame_h = camera.get(cv2.CAP_PROP_FRAME_HEIGHT) // 2
 canvas = tkinter.Canvas(main_windows, width=frame_w, height=frame_h)
 canvas.pack()
 
 photo = None
-
 counting = 0
 
 
@@ -23,11 +26,25 @@ def do_take_photo():
     counting += 1
 
 
+def enter_to_do(e):
+    global counting
+    counting += 1
+
+
 def update_frame():
     global photo, camera, counting
     ret, frame = camera.read()
+    cv2.rectangle(frame,
+                  (int(camera.get(cv2.CAP_PROP_FRAME_WIDTH) // 2 - 100),
+                   int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT) // 2 - 100)),
+                  (int(camera.get(cv2.CAP_PROP_FRAME_WIDTH) // 2 + 100),
+                   int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT) // 2 + 100)),
+                  (0, 255, 0), 3)
+    # resize frame fit to main_windows's canvas
     frame = cv2.resize(frame, dsize=None, fx=0.5, fy=0.5)
+    # convert color system
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
     # convert numpy image to image
     photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
     canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
@@ -38,22 +55,23 @@ def update_frame():
         return_value, image = camera.read()
         cv2.imwrite(f"hand_{counting}.png", image)
         image = cv2.imread(f"hand_{counting}.png")
-        # face = image[220:420, 140:340]
+        # crop frame for main file
         face = image[140:340, 220:420]
-        cv2.imwrite(f"final_{counting}.png", face)
+        # writing to path
+        cv2.imwrite(f"{hand_path}/final_{counting}.png", face)
+        # remove hands picture
         os.remove(f"hand_{counting}.png")
         counting += 1
+
     elif counting >= 5:
         main_windows.quit()
-#
-# # camera.release()
-# cv2.destroyAllWindows()
-# del camera  # so that others can use the camera as soon as possible
 
 
-button = tkinter.Button(main_windows, text='take photos', command=do_take_photo)
+button = tkinter.Button(main_windows, text='Press Enter or press the button here', activebackground='pale green',
+                        command=do_take_photo)
 button.pack()
 
+main_windows.bind('<Return>', enter_to_do)
 update_frame()
 main_windows.mainloop()
-print(counting)
+
