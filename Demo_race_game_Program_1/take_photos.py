2943cd319a27b1f50358d8df194f399c9ed02631
letrongminh <kimchon999555@gmail.com>
2021-05-13 03:25:25 +0300

    control button to take the photo. fix camera

--- Demo_race_game_Program_1/take_photos.py
+++ Demo_race_game_Program_1/take_photos.py
@@ -2,43 +2,58 @@ import cv2
 import time
 import os
 import tkinter
-
+import PIL.Image
+import PIL.ImageTk
 
 main_windows = tkinter.Tk(className='open camera')
+camera = cv2.VideoCapture(0)
+# main_windows.geometry('80x80')
+frame_w = camera.get(cv2.CAP_PROP_FRAME_WIDTH) // 2
+frame_h = camera.get(cv2.CAP_PROP_FRAME_HEIGHT) // 2
+canvas = tkinter.Canvas(main_windows, width=frame_w, height=frame_h)
+canvas.pack()
 
-main_windows.geometry('80x80')
+photo = None
+
+counting = 0
 
 
-def take_photo():
-    counting = 0
-    camera = cv2.VideoCapture(0)
-    while counting < 4:
-        ret, frame = camera.read()
-        if ret:
-            cv2.imshow('video', frame)
+def do_take_photo():
+    global counting
+    counting += 1
 
-        if cv2.waitKey(1) == ord('a'):
-            while counting < 4:
-                time.sleep(0.1)  # If don't wait, the image will be dark
-                counting += 1
-                return_value, image = camera.read()
-                cv2.imwrite(f"hand_{counting}.png", image)
 
-                image = cv2.imread(f"hand_{counting}.png")
+def update_frame():
+    global photo, camera, counting
+    ret, frame = camera.read()
+    frame = cv2.resize(frame, dsize=None, fx=0.5, fy=0.5)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # convert numpy image to image
+    photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+    canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
+    main_windows.after(1, update_frame)  # after 1 ms, refresh frame.
 
-                # face = image[220:420, 140:340]
-                face = image[140:340, 220:420]
-                cv2.imwrite(f"final_{counting}.png", face)
-                os.remove(f"hand_{counting}.png")
-        if cv2.waitKey(1) == ord('q'):
-            break
+    if 0 < counting < 4:
+        time.sleep(0.1)  # If don't wait, the image will be dark
+        return_value, image = camera.read()
+        cv2.imwrite(f"hand_{counting}.png", image)
+        image = cv2.imread(f"hand_{counting}.png")
+        # face = image[220:420, 140:340]
+        face = image[140:340, 220:420]
+        cv2.imwrite(f"final_{counting}.png", face)
+        os.remove(f"hand_{counting}.png")
+        counting += 1
+    elif counting > 4:
         main_windows.quit()
+#
+# # camera.release()
+# cv2.destroyAllWindows()
+# del camera  # so that others can use the camera as soon as possible
 
-    # camera.release()
-    cv2.destroyAllWindows()
-    del camera  # so that others can use the camera as soon as possible
 
+button = tkinter.Button(main_windows, text='take photos', command=do_take_photo)
+button.pack()
 
-B = tkinter.Button(main_windows, width=10, height=5, text="OK", bg='red', activebackground='red', command=take_photo)
-B.pack()
+update_frame()
 main_windows.mainloop()
+print(counting)
