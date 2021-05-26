f207bd0c3cf59315ddaf5b74e4f8ae7384e95d2a
letrongminh <kimchon999555@gmail.com>
2021-05-26 03:31:16 +0300

    fix docstring functions and etc...

--- Demo_race_game_Program_1/helper/mask_analysis.py
+++ Demo_race_game_Program_1/helper/mask_analysis.py
@@ -1,6 +1,5 @@
-import numpy as np
 import cv2
-import sys
+import numpy as np
 
 
 class BinaryMaskAnalyser:
@@ -12,16 +11,13 @@ class BinaryMaskAnalyser:
     """
 
     def returnNumberOfContours(self, mask):
-        """it returns the total number of contours present on the mask
- 
-        this method must be used during video analysis to check if the frame contains
-        at least one contour before calling the other function below.
-        @param mask the binary image to use in the function
-        @return get the number of contours 
+        """it returns the total number of contours present on the mask this method must be used during video analysis
+        to check if the frame contains at least one contour before calling the other function below. @parameter mask
+        the binary image to use in the function @return get the number of contours
         """
         if mask is None:
             return None
-        mask = np.copy(mask)  # doing a copy otherwise findContours modify the original(?)
+        mask = np.copy(mask)  # doing a copy otherwise findContours modify the original
         if len(mask.shape) == 3:
             mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
         contours, hierarchy = cv2.findContours(mask, 1, 2)
@@ -36,8 +32,7 @@ class BinaryMaskAnalyser:
         This method could be useful to find the center of a face when a skin detector filter is used.
         @parameter mask the binary image to use in the function
         @return get the x and y center coords of the contour whit the largest area.
-            In case of error it returns a tuple (None, None)
-        """
+            In case of error it returns a tuple (None, None)"""
         if mask is None:
             return None, None
         mask = np.copy(mask)
@@ -47,28 +42,25 @@ class BinaryMaskAnalyser:
         area_array = np.zeros(len(contours))  # contains the area of the contours
         counter = 0
         for cnt in contours:
-            # cv2.drawContours(image, [cnt], 0, (0,255,0), 3)
-            # print("Area: " + str(cv2.contourArea(cnt)))
             area_array[counter] = cv2.contourArea(cnt)
             counter += 1
         if area_array.size == 0:
             return None, None  # the array is empty
         max_area_index = np.argmax(area_array)  # return the index of the max_area element
-        # cv2.drawContours(image, [contours[max_area_index]], 0, (0,255,0), 3)
-        # Get the centre of the max_area element
+
         cnt = contours[max_area_index]
-        M = cv2.moments(cnt)  # calculate the moments
-        if M['m00'] == 0:
+        # calculate at the moments
+        moments = cv2.moments(cnt)
+        if moments['m00'] == 0:
             return None, None
-        cx = int(M['m10'] / M['m00'])  # get the center from the moments
-        cy = int(M['m01'] / M['m00'])
+        cx = int(moments['m10'] / moments['m00'])  # get the center from the moments
+        cy = int(moments['m01'] / moments['m00'])
         return cx, cy  # return the center coords
 
     def returnMaxAreaContour(self, mask):
         """it returns the contour with largest area.
- 
         This method could be useful to find a face when a skin detector filter is used.
-        @param mask the binary image to use in the function
+        @parameter mask the binary image to use in the function
         @return get the x and y center coords of the contour whit the largest area 
         """
         if mask is None:
@@ -80,8 +72,6 @@ class BinaryMaskAnalyser:
         area_array = np.zeros(len(contours))  # contains the area of the contours
         counter = 0
         for cnt in contours:
-            # cv2.drawContours(image, [cnt], 0, (0,255,0), 3)
-            # print("Area: " + str(cv2.contourArea(cnt)))
             area_array[counter] = cv2.contourArea(cnt)
             counter += 1
         if area_array.size == 0:
@@ -90,7 +80,7 @@ class BinaryMaskAnalyser:
         cnt = contours[max_area_index]
         return cnt  # return the max are contour
 
-    def drawMaxAreaContour(self, frame, mask, color=[0, 255, 0], thickness=3):
+    def drawMaxAreaContour(self, frame, mask, color=None, thickness=3):
         """it draws the contour with largest area.
  
         @parameter frame the image to use as canvas
@@ -98,24 +88,17 @@ class BinaryMaskAnalyser:
         @parameter color the color of the contour
         @parameter thickness of the contour
         """
+        if color is None:
+            color = [0, 255, 0]
         cnt = self.returnMaxAreaContour(mask)
         cv2.drawContours(frame, cnt, -1, color, thickness)
-
-    # TODO Return the orientation (clockwise or ccw) of a contour
-    # def returnMaxAreaOrientation():
-    # It can be done using the flag oriented to True.
-    # oriented – Oriented area flag. If it is true, the function returns
-    # a signed area value, depending on the contour orientation (clockwise or counter-clockwise).
-    # Using this feature you can determine orientation of a contour by taking the sign of an area.
-    # By default, the parameter is false, which means that the absolute value is returned.
-    # cv2.contourArea(contour[, oriented]) → retval
 
     def matchMaxAreaWithShape(self, mask, shape):
         """it returns a value which identify the similarity between the largest area contour and a shape.
  
-        The lower the result, the better match it is. It is calculated 
-        based on the hu-moment values. For example if we have three shapes:
-        A=star, B=rotated dilatated star, C=square
+        The lower the result, the better match it is. It is calculated based on the hu-moment values.
+        For example if we have three shapes:
+        A=star, B=rotated dilated star, C=square
         Matching Image A with itself = 0.0
         Matching Image A with Image B = 0.001946
         Matching Image A with Image C = 0.326911
@@ -128,30 +111,32 @@ class BinaryMaskAnalyser:
     def returnMaxAreaConvexHull(self, mask):
         """it returns the convex hull surrounding the contour with the largest area.
  
-        @param mask the binary image to use in the function
+        @parameter mask the binary image to use in the function
         @return get the coords of the convex hull
         """
         cnt = self.returnMaxAreaContour(mask)
         return cv2.convexHull(cnt)
 
-    def drawMaxAreaConvexHull(self, frame, mask, color=[0, 255, 0], thickness=3):
+    def drawMaxAreaConvexHull(self, frame, mask, color=None, thickness=3):
         """it draws the convex hull for the contour with largest area.
  
-        @param frame the image to use as canvas
-        @param mask the binary image to use in the function
-        @param color the color of the convex hull
-        @param thickness of the convex hull
+        @parameter frame the image to use as canvas
+        @parameter mask the binary image to use in the function
+        @parameter color the color of the convex hull
+        @parameter thickness of the convex hull
         """
+        if color is None:
+            color = [0, 255, 0]
         cnt = self.returnMaxAreaContour(mask)
         hull = cv2.convexHull(cnt)
         cv2.drawContours(frame, hull, -1, color, thickness)
 
     def returnMaxAreaRectangle(self, mask):
-        """it returns the rectangle sorrounding the contour with the largest area.
+        """it returns the rectangle surrounding the contour with the largest area.
  
         This method could be useful to find a face when a skin detector filter is used.
-        @param mask the binary image to use in the function
-        @return get the coords of the upper corner of the rectangle (x, y) and the rectangle size (widht, hight)
+        @parameter mask the binary image to use in the function
+        @return get the coords of the upper corner of the rectangle (x, y) and the rectangle size (width, height)
             In case of error it returns a tuple (None, None, None, None) 
         """
         if mask is None:
@@ -172,21 +157,23 @@ class BinaryMaskAnalyser:
         (x, y, w, h) = cv2.boundingRect(cnt)
         return x, y, w, h
 
-    def drawMaxAreaRectangle(self, frame, mask, color=[0, 255, 0], thickness=3):
+    def drawMaxAreaRectangle(self, frame, mask, color=None, thickness=3):
         """it draws the rectangle with largest area.
  
-        @param frame the image to use as canvas
-        @param mask the binary image to use in the function
-        @param color the color of the rectangle
-        @param thickness of the rectangle
+        @parameter frame the image to use as canvas
+        @parameter mask the binary image to use in the function
+        @parameter color the color of the rectangle
+        @parameter thickness of the rectangle
         """
+        if color is None:
+            color = [0, 255, 0]
         x, y, w, h = self.returnMaxAreaRectangle(mask)
         cv2.rectangle(frame, (x, y), (x + w, y + h), color, thickness)
 
     def returnMaxAreaCircle(self, mask):
-        """it returns the circle sorrounding the contour with the largest area.
+        """it returns the circle surrounding the contour with the largest area.
  
-        @param mask the binary image to use in the function
+        @parameter mask the binary image to use in the function
         @return get the center (x, y) and the radius of the circle
         """
         if mask is None:
@@ -207,13 +194,15 @@ class BinaryMaskAnalyser:
         (x, y), radius = cv2.minEnclosingCircle(cnt)
         return int(x), int(y), int(radius)
 
-    def drawMaxAreaCircle(self, frame, mask, color=[0, 255, 0], thickness=3):
+    def drawMaxAreaCircle(self, frame, mask, color=None, thickness=3):
         """it draws the circle with largest area.
  
-        @param frame the image to use as canvas
-        @param mask the binary image to use in the function
-        @param color the color of the circle
-        @param thickness of the circle
+        @parameter frame the image to use as canvas
+        @parameter mask the binary image to use in the function
+        @parameter color the color of the circle
+        @parameter thickness of the circle
         """
+        if color is None:
+            color = [0, 255, 0]
         x, y, r = self.returnMaxAreaCircle(mask)
         cv2.circle(frame, (x, y), r, color, thickness)
